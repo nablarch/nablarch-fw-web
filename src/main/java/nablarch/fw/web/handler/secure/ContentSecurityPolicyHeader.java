aa338d12904553e924f934ab8bc43ce377366834
kankichi00 <135567744+kankichi00@users.noreply.github.com>
2024-07-24 11:00:27 +0900

    CSPに対応できるようにするため、nonceを生成する機能を追加する (#132)
    * feat: SecureHandlerにCSP nonceを出力する設定の追加、ContentSecurityPolicyHeaderにポリシーを動的に設定できるように機能修正
    
    * docs:Javadocの記載
    
    * refactor:不要なメソッドの削除
    
    * refactor:inspection対応
    
    * test: デフォルト(generateCspNonce=false)の場合のテストを追加
    
    * test: inspection対応
    
    * docs: セキュアハンドラのJavadocの説明を修正
    
    * test: デフォルトの場合のテストに、nonceを生成するメソッドが呼ばれていないことの確認を追加
    
    * docs: Javadocで「。」がかけていたため追加
    
    ---------
    
    Co-authored-by: charon-r13b <matsushima.ippei@tis.co.jp>

--- src/main/java/nablarch/fw/web/handler/secure/ContentSecurityPolicyHeader.java
+++ src/main/java/nablarch/fw/web/handler/secure/ContentSecurityPolicyHeader.java
@@ -1,11 +1,13 @@
 package nablarch.fw.web.handler.secure;
 
+import nablarch.core.util.StringUtil;
 import nablarch.fw.web.HttpResponse;
+import nablarch.fw.web.handler.SecureHandler;
 import nablarch.fw.web.servlet.ServletExecutionContext;
 
 /**
  * Content-Security-Policyレスポンスヘッダを設定するクラス。
- *
+ * <p>
  * {@link #setReportOnly(boolean)} に{@code true}を設定した場合は、
  * Content-Security-Policy-Report-Onlyレスポンスヘッダを出力する。
  * 
@@ -18,6 +20,8 @@ public class ContentSecurityPolicyHeader implements SecureResponseHeader {
     private String policy;
     /** report-onlyモード */
     private boolean reportOnly;
+    /** プレースホルダー文字列 */
+    private final String cspNonceSourcePlaceHolder = "$cspNonceSource$";
 
     /**
      * Content-Security-Policyを設定する。
@@ -52,7 +56,27 @@ public class ContentSecurityPolicyHeader implements SecureResponseHeader {
         } else if (policy.isEmpty()) {
             throw new IllegalStateException("invalid Content-Security-Policy. policy is empty");
         }
+
         return policy;
+    }
+
+    /**
+     * セキュアハンドラでnonceが自動生成されている場合は、プレースホルダーをnonceに置換する。
+     * 自動生成されていない場合は、プレースホルダーをそのまま返す。
+     *
+     * @param context 実行コンテキスト
+     * @return レスポンスヘッダの値
+     */
+    public String getFormattedValue(ServletExecutionContext context) {
+        String rawPolicy = getValue();
+
+        String nonce = context.getRequestScopedVar(SecureHandler.CSP_NONCE_KEY);
+
+        if (StringUtil.isNullOrEmpty(nonce)) {
+            return rawPolicy;
+        }
+
+        return rawPolicy.replace(cspNonceSourcePlaceHolder, "nonce-" + nonce);
     }
 
     /**
--- src/test/java/nablarch/fw/web/handler/SecureHandlerTest.java
+++ src/test/java/nablarch/fw/web/handler/SecureHandlerTest.java
@@ -2,7 +2,7 @@ package nablarch.fw.web.handler;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -11,7 +11,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mockit.Verifications;
+import nablarch.core.util.Base64Util;
+import nablarch.fw.web.handler.secure.ContentSecurityPolicyHeader;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.collection.IsMapContaining;
 
 import nablarch.fw.ExecutionContext;
@@ -47,15 +52,12 @@ public class SecureHandlerTest {
     @Mocked
     private HttpRequest mockHttpRequest;
 
-    /** テスト対象 */
-    private SecureHandler sut = new SecureHandler();
-
     private ServletExecutionContext context;
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    private Handler<HttpRequest, HttpResponse> handler = new Handler<HttpRequest, HttpResponse>() {
+    private final Handler<HttpRequest, HttpResponse> handler = new Handler<HttpRequest, HttpResponse>() {
         @Override
         public HttpResponse handle(final HttpRequest request, final ExecutionContext context) {
             return new HttpResponse(200);
@@ -70,6 +72,7 @@ public class SecureHandlerTest {
             mockServletRequest.getContextPath();
             result = "sampleapp";
         }};
+
         context = new ServletExecutionContext(mockServletRequest, mockServletResponse, mockServletContext);
         context.addHandler(handler);
     }
@@ -79,6 +82,7 @@ public class SecureHandlerTest {
      */
     @Test
     public void defaultSettings() {
+        final SecureHandler sut = new SecureHandler();
 
         final HttpResponse result = sut.handle(mockHttpRequest, context);
 
@@ -89,17 +93,20 @@ public class SecureHandlerTest {
                 IsMapContaining.hasEntry("X-XSS-Protection", "1; mode=block"),
                 IsMapContaining.hasEntry("X-Content-Type-Options", "nosniff"),
                 IsMapContaining.hasEntry("Referrer-Policy", "strict-origin-when-cross-origin"),
-                IsMapContaining.hasEntry("Cache-Control", "no-store")
+                IsMapContaining.hasEntry("Cache-Control", "no-store"),
+                not(IsMapContaining.hasKey("Content-Security-Policy"))
         ));
         assertNull(result.getHeaderMap().get("Content-Type"));
+
 
     }
 
     /**
-     * 出力対象外の場合、そのヘッダは出力されないこと
+     * 出力対象外の場合、そのヘッダは出力されないこと。
      */
     @Test
     public void withoutFrameOptions() {
+        final SecureHandler sut = new SecureHandler();
 
         final FrameOptionsHeader frameOption = new FrameOptionsHeader();
         frameOption.setOption("NONE");
@@ -110,5 +117,75 @@ public class SecureHandlerTest {
         assertThat(response.getHeaderMap(), CoreMatchers.<Map<String, String>>allOf(
                 IsMapContaining.hasEntry("X-XSS-Protection", "1; mode=block"),
                 not(IsMapContaining.hasKey("X-Frame-Options"))));
+    }
+
+    /**
+     * generateCspNonceをtrueにしている場合、リクエストスコープに生成されたnonceが保存されること。
+     * またContentSecurityPolicyHeaderを設定している場合は、プレースホルダーが生成されたnonceで置換されること。
+     */
+    @Test
+    public void enableGenerateCspNonce() {
+        new Expectations() {{
+            mockServletRequest.getAttribute(SecureHandler.CSP_NONCE_KEY);
+            result = "abcde";
+        }};
+
+        final SecureHandler sut = new SecureHandler();
+        sut.setGenerateCspNonce(true);
+
+        final ContentSecurityPolicyHeader contentSecurityPolicy = new ContentSecurityPolicyHeader();
+        contentSecurityPolicy.setPolicy("script-src 'self' '$cspNonceSource$'; style-src '$cspNonceSource$'");
+        sut.setSecureResponseHeaderList(Arrays.asList(new XssProtectionHeader(), contentSecurityPolicy));
+
+        final HttpResponse response = sut.handle(mockHttpRequest, context);
+
+        assertThat(response.getHeaderMap(), CoreMatchers.<Map<String, String>>allOf(
+                IsMapContaining.hasEntry("X-XSS-Protection", "1; mode=block"),
+                IsMapContaining.hasEntry("Content-Security-Policy", "script-src 'self' 'nonce-abcde'; style-src 'nonce-abcde'")
+        ));
+
+        new Verifications() {{
+            mockServletRequest.setAttribute(SecureHandler.CSP_NONCE_KEY, withArgThat(new TypeSafeMatcher<String>() {
+                @Override
+                protected boolean matchesSafely(String item) {
+                    byte[] binary = Base64Util.decode(item);
+                    assertThat(binary.length, is(16));
+
+                    return true;
+                }
+
+                @Override
+                public void describeTo(Description description) {
+                    description.appendText("Base64でエンコードされた16バイト（128ビット）の文字列をnonceとして指定する必要があります");
+                }
+            }));
+            times = 1;
+        }};
+    }
+
+    /**
+     * デフォルト設定の場合、nonceが生成されないこと。
+     */
+    @Test
+    public void disableGenerateCspNonce(){
+
+        final SecureHandler sut = new SecureHandler();
+        assertThat(sut.isGenerateCspNonce(), is(false));
+
+        final ContentSecurityPolicyHeader contentSecurityPolicy = new ContentSecurityPolicyHeader();
+        contentSecurityPolicy.setPolicy("script-src 'self' '$cspNonceSource$'; style-src '$cspNonceSource$'");
+        sut.setSecureResponseHeaderList(Arrays.asList(new XssProtectionHeader(), contentSecurityPolicy));
+
+        final HttpResponse response = sut.handle(mockHttpRequest, context);
+
+        assertThat(response.getHeaderMap(), CoreMatchers.<Map<String, String>>allOf(
+                IsMapContaining.hasEntry("X-XSS-Protection", "1; mode=block"),
+                IsMapContaining.hasEntry("Content-Security-Policy", "script-src 'self' '$cspNonceSource$'; style-src '$cspNonceSource$'")
+        ));
+
+        new Verifications() {{
+            mockServletRequest.setAttribute(SecureHandler.CSP_NONCE_KEY, any);
+            times = 0;
+        }};
     }
 }
