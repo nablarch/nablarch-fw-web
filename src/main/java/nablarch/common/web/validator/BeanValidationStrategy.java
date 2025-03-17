468df1d270454a30e22c7fc48f50cf9a5b7396b6
kankichi <ichikawa.kankichi@tis.co.jp>
2025-03-17 11:38:24 +0900

    refactor: 明示的な型は<>に統一（Inspection対応）

--- src/main/java/nablarch/common/web/validator/BeanValidationStrategy.java
+++ src/main/java/nablarch/common/web/validator/BeanValidationStrategy.java
@@ -3,7 +3,6 @@ package nablarch.common.web.validator;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -110,7 +109,7 @@ public class BeanValidationStrategy implements ValidationStrategy {
         @SuppressWarnings("unchecked")
         final List<String> parameterNames = Collections.list(request.getParameterNames());
 
-        final List<Message> sortedMessage = new ArrayList<Message>(messages);
+        final List<Message> sortedMessage = new ArrayList<>(messages);
         Collections.sort(sortedMessage, (m1, m2) -> {
             final int index1 = getParameterIndex(parameterNames, m1);
             final int index2 = getParameterIndex(parameterNames, m2);
@@ -155,12 +154,12 @@ public class BeanValidationStrategy implements ValidationStrategy {
     private Map<String, String[]> getMapWithConvertedKey(String prefix, Map<String, String[]> reqParamMap) {
         if (StringUtil.isNullOrEmpty(prefix)) {
             // プレフィックスが指定されない場合、全てが対象とする
-            return new HashMap<String, String[]>(reqParamMap);
+            return new HashMap<>(reqParamMap);
         }
 
         final String prefixName = prefix + '.';
         final int prefixLength = prefixName.length();
-        Map<String, String[]> convertedMap = new HashMap<String, String[]>();
+        Map<String, String[]> convertedMap = new HashMap<>();
         for (Map.Entry<String, String[]> entry : reqParamMap.entrySet()) {
             final String key = entry.getKey();
             final String[] value = entry.getValue();
